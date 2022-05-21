defbb98e92d5ad4a63fe0a712982b69ac2229ffe
Kranthi Pamulapati <pamulapati.kranthi@gmail.com>
2022-05-21 09:18:20 +0530

    Vanilla JS merge

---
+++ VanillaJS/js/app.js
@@ -0,0 +1,11 @@
+const onLoad = () => {
+    hideLoader();
+}
+
+const showLoader = () => {
+    document.getElementById("loader").hidden = false;
+}
+
+const hideLoader = () => {
+    document.getElementById("loader").hidden = true;
+}
